8b1c0860873db90c250d0d04a5eea49556208ed2
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2025-01-17 07:46:16 +0800

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import GoogleAdsense from "@/components/adsense";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
                 className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#F8F9FA]`}
             >
                 {children}
+                <GoogleAnalytics gaId="G-YLVVD3KLEL" />
             </body>
         </html>
     );

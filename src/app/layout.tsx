60d4b55b8c964e47e311760f7c3089505c85d8fd
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2025-01-18 15:21:43 +0800

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import GoogleAdsense from "@/components/adsense";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
